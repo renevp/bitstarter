8e94a2d452dd85f96e571d6d7fcc693393690072
Rene Pereira <rene.vallenilla@gmail.com>
2013-07-10 13:51:57 +0000

    Allright

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+var fs = require('fs');
+var inputfile = "index.html";
+
+var buffer = fs.readFileSync(inputfile);
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+    response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
